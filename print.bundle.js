a49c9f6c2c7655d7f3ccda6ec5594e4a1054eace
Sergey Timofeev <drumliker@yandex.ru>
2026-08-09 12:30:11 +0300

    Updates

---
+++ print.bundle.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunklesson26_webpack"] = self["webpackChunklesson26_webpack"] || []).push([[568],{
+
+/***/ 258
+() {
+
+function printMe() {
+  for (let index = 0; index < 100; index++) {
+    console.log(index);
+  }
+  console.log("I get called from print.js!");
+}
+
+printMe();
+
+
+/***/ }
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__(258));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJpbnQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFBO0FBQ0Esc0JBQXNCLGFBQWE7QUFDbkM7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbGVzc29uMjZfd2VicGFjay8uL3NyYy9wcmludC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBwcmludE1lKCkge1xyXG4gIGZvciAobGV0IGluZGV4ID0gMDsgaW5kZXggPCAxMDA7IGluZGV4KyspIHtcclxuICAgIGNvbnNvbGUubG9nKGluZGV4KTtcclxuICB9XHJcbiAgY29uc29sZS5sb2coXCJJIGdldCBjYWxsZWQgZnJvbSBwcmludC5qcyFcIik7XHJcbn1cclxuXHJcbnByaW50TWUoKTtcclxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
